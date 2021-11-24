3ffccc24a2be919ceb5e5ee09b82df5f469b802e
Mubarak <imubarak2424@gmail.com>
2021-11-24 12:44:06 +0100

    class features

--- books.js
+++ books.js
@@ -1,18 +1,137 @@
-const bookList = [];
-
-class Books {
-  constructor(title, author) {
+/* eslint-disable max-classes-per-file */
+/* eslint-disable no-unused-vars */
+class Book2 {
+  constructor(title, author, next_node = null) {
     this.title = title;
     this.author = author;
   }
 }
 
+class BookList2 {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+
+  add(title, author) {
+    const newnode = new Book2(title, author);
+    if (this.tail === null) {
+      this.head = newnode;
+      this.tail = newnode;
+    } else {
+      this.tail.next_node = newnode;
+      this.tail = newnode;
+    }
+  }
+
+  size() {
+    let count = 0;
+    let node = this.head;
+    while (node) {
+      count += 1;
+      node = node.next_node;
+    }
+    return count;
+  }
+
+  print() {
+    let nodes = this.head;
+    while (nodes) {
+      nodes = nodes.next_node;
+      return `${nodes.title} ${nodes.author}`;
+    }
+    return null;
+  }
+
+  printArray() {
+    const arr = [];
+    let node = this.head;
+    while (node) {
+      const obj = {
+        title: node.title,
+        author: node.author,
+      };
+      arr.push(obj);
+      node = node.next_node;
+    }
+    return arr;
+  }
+
+  get(index) {
+    let node = this.head;
+    let count = 0;
+    while (node) {
+      if (index === count) { return `${node.title} ${node.author}`; }
+      node = node.next_node;
+      count += 1;
+    }
+    return null;
+  }
+
+  remove(number) {
+    let node = this.head;
+    let node2 = this.head;
+    let newnode = this.head;
+    let count = 0;
+    while (node) {
+      if (number === count) {
+        newnode = node.next_node;
+      }
+      node = node.next_node;
+      count += 1;
+    }
+
+    count = 0;
+    while (node2) {
+      if (count === number - 1) {
+        node2.next_node = newnode;
+      } else if (number === 0) {
+        this.head = node2.next_node;
+        break;
+      }
+      node2 = node2.next_node;
+      count += 1;
+    }
+
+    if (this.head === undefined) {
+      this.head = null;
+      this.tail = null;
+    }
+  }
+
+  addAt(index, title, author) {
+    const newnode = new Book2(title, author);
+    let count = 0;
+    let node = this.head;
+    if (this.tail === null) {
+      this.head = newnode;
+      this.tail = newnode;
+    } else {
+      while (node) {
+        if (count === index - 1) {
+          newnode.next_node = node.next_node;
+          node.next_node = newnode;
+        } else if (index === 0) {
+          newnode.next_node = this.head;
+          this.head = newnode;
+          break;
+        }
+        count += 1;
+        node = node.next_node;
+      }
+    }
+  }
+}
+
+const lists = new BookList2();
+
 const section = document.querySelector('.bookings');
 let count = 0;
 
 function addBook(Title, Author) {
-  const booky = new Books(Title, Author);
-  bookList.push(booky);
+  const booky = new Book2(Title, Author);
+
+  lists.add(booky.title, booky.author);
 
   const container = document.createElement('div');
   const names = document.createElement('h3');
@@ -41,9 +160,10 @@ function addBook(Title, Author) {
 }
 
 function removeList(name, author) {
-  for (let x = 0; x < bookList.length; x += 1) {
-    if ((bookList[x].title === name) && (bookList[x].author === author)) {
-      bookList.splice(x, 1);
+  const arr = lists.printArray();
+  for (let x = 0; x < arr.length; x += 1) {
+    if ((arr[x].title === name) && (arr[x].author === author)) {
+      lists.remove(x);
       break;
     }
   }
@@ -77,12 +197,12 @@ forms.addEventListener('submit', (event) => {
 });
 
 function populate() {
-  const instances = {
-    figures: [],
+  const instances1 = {
+    figures1: [],
   };
 
-  instances.figures.push(...bookList);
-  localStorage.setItem('instances', JSON.stringify(instances));
+  instances1.figures1.push(lists);
+  localStorage.setItem('instances1', JSON.stringify(instances1));
 }
 
 next.addEventListener('click', () => {
@@ -99,9 +219,15 @@ next.addEventListener('click', () => {
 });
 
 function setForm() {
-  const restore = JSON.parse(localStorage.getItem('instances'));
-  if (restore) {
-    restore.figures.forEach((adding) => {
+  const restore1 = JSON.parse(localStorage.getItem('instances1'));
+  const newList = new BookList2();
+  if (restore1 != null) {
+    newList.head = restore1.figures1[0].head;
+    newList.tail = restore1.figures1[0].tail;
+  }
+
+  if (restore1) {
+    newList.printArray().forEach((adding) => {
       addBook(adding.title, adding.author);
     });
   }
